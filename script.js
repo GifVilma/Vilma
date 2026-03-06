71eebf60bab31dae968863789915490cfd40a7e8
Vilma Anasicha <curso.vilma.italia@gmail.com>
2026-03-06 03:26:29 +0100

    Actualización sección contacto

--- script.js
+++ script.js
@@ -3,4 +3,3 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
     alert('¡Gracias por tu mensaje, Vilma se pondrá en contacto contigo pronto!');
     this.reset();
 });
-
