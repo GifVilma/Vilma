81d3cb2566475dcc1989b10a9d526e99e93e937f
Vilma Anasicha <curso.vilma.italia@gmail.com>
2026-03-06 01:21:19 +0100

    Primer commit del proyecto

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('¡Gracias por tu mensaje, Vilma se pondrá en contacto contigo pronto!');
+    this.reset();
+});
+
